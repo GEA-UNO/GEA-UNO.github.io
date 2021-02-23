4c7eaa451d17d5d2ef08e3dfbad4ad671b1b956e
Igna Garcia <naachxp@gmail.com>
2021-02-23 00:26:59 -0300

    then-catch

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const url = "http://igna98.alwaysdata.net/link/";
 
 function increment(name){
     fetch(`${url}${name}`, { method: "PATCH" })
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
 }
