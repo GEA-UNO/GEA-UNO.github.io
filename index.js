ec9b9d8b59717213f92e3c727723f8cb4ba69451
Igna Garcia <naachxp@gmail.com>
2021-02-23 00:38:42 -0300

    redirect follow

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const url = "https://igna98.alwaysdata.net/link/";
 
 function increment(name){
-    fetch(`${url}${name}`, { method: "PATCH" })
+    fetch(`${url}${name}`, { method: "PATCH", redirect: "follow" })
         .then(res => console.log(res))
         .catch(err => console.log(err))
 }
