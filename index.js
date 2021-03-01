cad1f12a9061be50184306865a222dcb493e01cd
Fernando Pastorelli <fernando.n.p.d.moscu@gmail.com>
2021-03-01 20:39:26 -0300

    Fixed?

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const url = "https://igna98.alwaysdata.net/link/";
 
 function increment(name){
-    fetch(`${url}${name}`, { method: "PATCH", redirect: "follow" })
+    window.onbeforeunload = function() {
+        fetch(`${url}${name}`, { method: "PATCH", redirect: "manual" })
         .then(res => console.log(res))
         .catch(err => console.log(err))
+    }
 }
