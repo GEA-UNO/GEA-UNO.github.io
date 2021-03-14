e8212ef007f52f1014699b87ecd042813ec8ed92
Igna Garcia <naachxp@gmail.com>
2021-03-14 16:23:52 -0300

    onload event

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const url = "https://igna98.alwaysdata.net/";
 
 function increment(name){
+        console.log(name)
         fetch(`${url}${name}`, { method: "PATCH", redirect: "manual" })
         .then(res => console.log(res))
         .catch(err => console.log(err))
