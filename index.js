7ea7bc49bde4ae8ba7ed334ece07333e8de89eb3
Igna Garcia <gnachoxp@gmail.com>
2021-06-03 00:45:17 -0300

    add function to get links

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const url = "https://igna98.alwaysdata.net/";
 
-function increment(name){
+const increment = (name) => {
         fetch(`${url}${name}`, { method: "PATCH", redirect: "manual" })
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
+                .then(res => console.log(res))
+                .catch(err => console.log(err))
+}
+
+const getLinks = (name) => {
+        fetch(`${url}link?father=${name}`, { method: "GET"})
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .catch(err => console.log(err))
+}
+
+const initPage = (pageTag, pagePath) => {
+        increment(pageTag);
+        getLinks(pagePath);
 }
